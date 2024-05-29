28722975ed0a05d9386ece20e7ebfd5e0b7d432c
Jan Dobes <dobesjan97@gmail.com>
2024-05-29 17:36:43 +0200

    remove unecessary

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,7 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
-import Auth0ProviderWithHistory from './components/auth0.jsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
